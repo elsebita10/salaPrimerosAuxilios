3099e0641f4b42302cbd2e194eeba05840b5c29d
juliaesc <juliaescobio@gmail.com>
2017-06-24 18:36:33 -0300

    Arreglo en el script. Se modifica: * Se agrega un usuario ADMIN corregido. * Se agrega un usuario USER.

--- src/main/resources/db/scriptInicialPrimerosAuxiliosApp.sql
+++ src/main/resources/db/scriptInicialPrimerosAuxiliosApp.sql
@@ -62,13 +62,19 @@ VALUES ('ADMIN');
   
 /* Populate one Admin User which will further create other users for the application using GUI */
 INSERT INTO USERS(username, password, address, email)
-VALUES ('sala1','abc123', '143 y 66','sala1@buildingways.com.ar');
+VALUES ('seba','abc123', '143 y 66','seba@buildingways.com.ar');
   
+INSERT INTO USERS(username, password, address, email)
+VALUES ('sala1','abc123', '143 y 66','sala1@buildingways.com.ar');
   
 /* Populate JOIN Table */
 INSERT INTO USERS_USER_PROFILES (user_id, user_profile_id)
   SELECT user.id, profile.id FROM users user, user_profiles profile
-  where user.username='bw' and profile.type='ADMIN';
+  where user.username='seba' and profile.type='ADMIN';
+  
+INSERT INTO USERS_USER_PROFILES (user_id, user_profile_id)
+  SELECT user.id, profile.id FROM users user, user_profiles profile
+  where user.username='sala1' and profile.type='USER';
  
 /* Create persistent_logins Table used to store rememberme related stuff*/
 CREATE TABLE PERSISTENT_LOGINS (
