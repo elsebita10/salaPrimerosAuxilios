e1f30c9ac15b7d47ba6eb2ff36e0fd647a60962a
juliaesc <juliaescobio@gmail.com>
2017-06-24 23:17:37 -0300

    Actualización de la página HOME:
    * Se corrigió la ruta para las imágenes.
    * Se agregó el mapeo a la carpeta de imágenes en la clase AppConfig.

--- src/main/java/ar/com/buildingways/salaprimerosauxilios/configuration/AppConfig.java
+++ src/main/java/ar/com/buildingways/salaprimerosauxilios/configuration/AppConfig.java
@@ -45,6 +45,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
     }
      
     /**
